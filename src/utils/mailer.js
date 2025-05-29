84b2b505a125c31cc108deb91f956c5894617acd
sagarpoudel638 <sagarpoudel638@gmail.com>
2025-05-29 21:31:14 +1000

    added official email to mailer

--- src/utils/mailer.js
+++ src/utils/mailer.js
@@ -2,7 +2,7 @@ import nodemailer from "nodemailer";
 
 // Create the transporter
 const transporter = nodemailer.createTransport({
-  host: "smtp.ethereal.email",
+  host: "smtp.zoho.com.au",
   port: 587,
   secure: false, // true for 465, false for other ports
   auth: {
